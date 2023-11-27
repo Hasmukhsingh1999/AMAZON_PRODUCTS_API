9f8c28f120498e44eef8aacd1ae99d9502d037cf
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-27 14:07:57 +0530

    added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json()); // Use express.json() to enable JSON body parsing
+app.use(express.urlencoded({ extended: true }));
 
+const PORT = process.env.PORT || 4000;
 
-
-const PORT = process.env.PORT || 4000
-app.listen(PORT,()=>{
-    console.log(`Server started at ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
